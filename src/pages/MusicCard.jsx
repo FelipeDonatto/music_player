3012bee402ae3af5b9d6e97018fec59ed50c7424
Felipe <fe.donatto@outlook.com>
2022-07-04 18:31:25 -0300

    8 ✔️

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,26 +1,83 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import getMusics from '../services/musicsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      favSong: false,
+    };
+  }
+
+  // componentDidUpdate() {
+  //   const { favSong } = this.state;
+  //   const { trackId } = this.props;
+  //   if (favSong) {
+  //     this.saveSong(trackId);
+  //   }
+  // }
+
+  loadingSwitch = () => {
+    this.setState((prevState) => ({
+      loading: !prevState.loading,
+    }));
+  }
+
   render() {
     const {
       previewUrl,
       trackName,
+      trackId,
     } = this.props;
+    const { loading, favSong } = this.state;
+
+    const saveSong = async (id) => {
+      this.loadingSwitch();
+      let music = '';
+      await getMusics(id)
+        .then(async (result) => { music = result[0].trackId; });
+      await addSong(music);
+      this.loadingSwitch();
+
+      this.setState((prevState) => ({
+        favSong: !prevState.favSong,
+      }));
+    };
+
+    if (loading) return <Loading />;
     return (
-      <div className="music">
-        <p>
+      <>
+        <div className="musicName">
           {' '}
-          {trackName }
+          {trackName}
           {' '}
-        </p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-          .
-        </audio>
-      </div>
+        </div>
+        <div className="music">
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            <code>audio</code>
+            .
+          </audio>
+          <label
+            htmlFor={ trackId }
+          >
+            <input
+              data-testid={ `checkbox-music-${trackId}` }
+              type="checkbox"
+              id={ trackId }
+              checked={ favSong }
+              onClick={ async () => saveSong(trackId) }
+            />
+            Favorita
+          </label>
+        </div>
+
+      </>
     );
   }
 }
@@ -28,5 +85,6 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   previewUrl: PropTypes.string.isRequired,
   trackName: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
 export default MusicCard;
